b0a4c0b667a24e759ebdc3e51ae35c5ae1bb891e
Keith Armstrong <keith1015@gmail.com>
2015-10-26 19:29:24 -0700

    Implemented demo slack app

--- apps/slack/app.js
+++ apps/slack/app.js
@@ -3,26 +3,23 @@ var request = require('request');
 var config = require('../../config.json');
 
 module.exports.post = function(text, req, res) {
-  console.log(text);
-  var token = JSON.parse(body).access_token;
+  var token = "";
   var channels;
-  var general;
-/*
+  var channel;
+
   request("https://slack.com/api/channels.list?token=" + token, function (error, response, body) {
     channels = JSON.parse(body).channels;
 
     _.each(channels, function(value) {
       if (value.name === "general") {
-        general = value.id;
-        return;
+        channel = value.id;
       }
     });
-    console.log(token);
+    console.log(channel);
 
-    request.get("https://slack.com/api/chat.postMessage?token=" + token + "&channel=" + general + "&text=testing 1 2 3&as_user=true", function (error, response, body) {
-      console.log(error, body);
+    request.get("https://slack.com/api/chat.postMessage?token=" + token + "&channel=" + channel + "&text=" + text + "&as_user=true&link_names=1", function (error, response, body) {
       res.send(200);
     });
   });
-*/
+
 };
