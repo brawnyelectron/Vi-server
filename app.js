9262c5f8bcb12050aa0161bf72ccee7abcdb3db3
Garrett Maring <garrett.maring@gmail.com>
2015-10-27 17:03:04 -0700

    Route bugs fixed

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var oauth = require('./routes/oauth');
+var db = require('./routes/db');
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/oauth', oauth);
+app.use('/db', db);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
