3cc3ca46948eda74159b3b3d68fab0a023aae694
Keith Armstrong <keith1015@gmail.com>
2015-10-30 11:08:39 -0700

    Zip files up to 50mb can be accepted

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.set('view engine', 'jade');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(bodyParser.raw({ type: 'application/zip' }));
+app.use(bodyParser.raw({ type: 'application/zip', limit: '50mb' }));
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: false}));
 app.use(cookieParser());
