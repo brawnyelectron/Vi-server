0f13be0e20e11c7cf14898f9f8790e44025ba4bd
Keith Armstrong <keith1015@gmail.com>
2015-10-30 16:32:14 -0700

    Changed username to email and hashed passwords in Developer Schema

--- routes/developer.js
+++ routes/developer.js
@@ -1,13 +1,14 @@
-var express = require('express');
-var router = express.Router();
-var utils = require('../utils');
-var mongoose = require('mongoose');
+var express   = require('express');
+var router    = express.Router();
+var mongoose  = require('mongoose');
+var bcrypt    = require('bcrypt');
+var utils     = require('../utils');
 
 var Developer = require('../db/Developer');
 
 router.post('/', function(req, res) {
-  var developer = new Developer({ username: req.body.username, password: req.body.password });
-  Developer.update({ username: req.body.username }, developer, { upsert: true }, function(err, dev) {
+  var developer = new Developer({ email: req.body.email, password: bcrypt.hashSync(req.body.password, 8) });
+  Developer.update({ email: req.body.email}, developer, { upsert: true }, function(err, dev) {
     if (err) { 
       console.log('Couldn\'t add dev! Error: ', err);
     }
