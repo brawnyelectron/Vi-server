d757462aa547033fd3e9589339aad1912c8e19b9
Keith Armstrong <keith1015@gmail.com>
2015-10-31 17:20:26 -0700

    Added developer login/registration logic

--- db/Developer.js
+++ db/Developer.js
@@ -3,6 +3,7 @@ var mongoose = require('mongoose');
 var developerSchema = mongoose.Schema({
   email: String,
   password: String,
+  authToken: String,
 });
 
 var Developer = mongoose.model('Developer', developerSchema);
--- routes/developer.js
+++ routes/developer.js
@@ -2,18 +2,35 @@ var express   = require('express');
 var router    = express.Router();
 var mongoose  = require('mongoose');
 var bcrypt    = require('bcrypt');
+var uuid      = require('node-uuid');
 var utils     = require('../utils');
 
 var Developer = require('../db/Developer');
 
 router.post('/', function(req, res) {
-  var developer = new Developer({ email: req.body.email, password: bcrypt.hashSync(req.body.password, 8) });
-  Developer.update({ email: req.body.email }, developer, { upsert: true }, function(err, dev) {
-    if (err) { 
-      console.log('Couldn\'t add dev! Error: ', err);
+  var email = req.body.email;
+  var password = req.body.password;
+
+  // Check if email exists in the database
+  Developer.findOne({email: email}, function(err, developer) {
+
+    // Account exists
+    if (developer !== null) {
+      if (bcrypt.compareSync(password, developer.password)) {
+        var guid = uuid.v4();
+        Developer.findOneAndUpdate({email: email}, {authToken: guid}, function(err, a) {
+          res.status(200).send(guid);
+        });
+      } else {
+        res.status(401).send("Email exists but you typed the wrong password so bad on you!")
+      }
+    } else {
+      var guid = uuid.v4();
+      var developer = new Developer({ email: email, password: bcrypt.hashSync(password, 8), authToken: guid });
+      developer.save();
+      res.status(201).send(guid);
     }
   });
-  res.sendStatus(200);
 });
 
 module.exports = router;
